6352a628b72a1b066c87503b65637cb39c838fe5
Dustin Chabrowski <dustin.duske@gmail.com>
2018-01-19 23:16:58 +0100

    [FEATURE] add simple generic collision detection

--- org.xtext.example.mgpl/res/src/framework/Collision.ts
+++ org.xtext.example.mgpl/res/src/framework/Collision.ts
@@ -1,5 +1,7 @@
 import Circle from "./Circle.js";
 import Rectangle from "./Rectangle.js";
+import Triangle from "./Triangle.js";
+import { CollisionObject, Boundaries } from './Types.js';
 
 export function touches(circle: Circle, player: Rectangle): boolean {
     const playerRightEdge = player.x + player.width;
@@ -10,5 +12,31 @@ export function touches(circle: Circle, player: Rectangle): boolean {
     if(circle.x <= playerRightEdge && circleBottom >= playerTop && circleTop <= playerBottom) {
         return true;
     }
+    return false;
+}
+
+export function touches2(a: CollisionObject, b: CollisionObject): boolean {
+    const aDim = a.getBoundaries();
+    const bDim = b.getBoundaries();
+    // touch right edge
+    if(aDim.x <= (bDim.x + bDim.width) && (aDim.y + aDim.height) >= bDim.y && aDim.y <= (bDim.y + bDim.height)) {
+        return true;
+    }
+
+    // touch left edge
+    if((aDim.x + aDim.width) >= bDim.x && (aDim.y + aDim.height) >= bDim.y && aDim.y <= (bDim.y + bDim.height)) {
+        return true;
+    }
+
+    // touch top edge
+    if((aDim.y + aDim.height) >= bDim.y && (aDim.x + aDim.width) >= bDim.x && aDim.x <= (bDim.x + bDim.width)) {
+        return true;
+    }
+
+    // touch bottom edge
+    if((aDim.y) <= (bDim.y + bDim.height) && (aDim.x + aDim.width) >= bDim.x && aDim.x <= (bDim.x + bDim.width)) {
+        return true;
+    }
+
     return false;
 }
